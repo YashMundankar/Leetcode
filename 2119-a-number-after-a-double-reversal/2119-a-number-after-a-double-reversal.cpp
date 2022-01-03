c2c59be2781df536a3180e042582c739549d8c62
Yash Mundankar <90042941+YashMundankar@users.noreply.github.com>
2022-01-03 21:20:33 +0530

    Time: 6 ms (5.11%), Space: 5.8 MB (70.29%) - LeetHub

--- 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.cpp
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.cpp
@@ -1,18 +1,7 @@
 class Solution {
 public:
     bool isSameAfterReversals(int num) {
-        if(num==0){
-            return true;
-        }
-        if(num%2==0)
-        {
-            float temp=num/5.00;
-            if(temp==floor(temp)){
-                return false;
-        }
-        }
-        
-        return true;
+       return num==0||num%10;
         
     }
 };
