df6f0b339427db909104db36e86c8a2af1e59a16
Yash Mundankar <90042941+YashMundankar@users.noreply.github.com>
2022-01-09 13:06:00 +0530

    Time: 37 ms (5.18%), Space: 10.1 MB (65.43%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int count_bits(int num){
+    static int count_bits(int num){
         int count=0;
         while(num){
             if(num&1){
@@ -11,24 +11,16 @@ public:
         return count;
     }
     
-    static bool func(pair<int ,int>p1,pair<int,int>p2){
-        if(p1.first==p2.first){
-            return p1.second<p2.second;
+    static bool func(int p1,int p2){
+        if(count_bits(p1)==count_bits(p2)){
+            return p1<p2;
         }
-        return p1.first<p2.first;
+        return count_bits(p1)<count_bits(p2);
     }
    
     vector<int> sortByBits(vector<int>& arr) {
-        vector<pair<int,int>>m1;
-        for(auto i : arr){
-            m1.push_back({count_bits(i),i});
-        }
         
-        sort(m1.begin(),m1.end(),func);
-        vector<int>ans;
-        for(auto i: m1){
-            ans.push_back(i.second);
-        }
-        return ans;
+        sort(arr.begin(),arr.end(),func);
+        return arr;
     }
 };
