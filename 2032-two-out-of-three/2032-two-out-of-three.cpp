9dc9e4026ca1b667b701da461b7cabf720bf28b8
Yash Mundankar <90042941+YashMundankar@users.noreply.github.com>
2022-01-10 18:02:16 +0530

    Time: 8 ms (97.26%), Space: 25.4 MB (83.27%) - LeetHub

--- 2032-two-out-of-three/2032-two-out-of-three.cpp
+++ 2032-two-out-of-three/2032-two-out-of-three.cpp
@@ -2,27 +2,12 @@ class Solution {
 public:
     vector<int> twoOutOfThree(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3) {
         vector<int>a(101,0);
-        vector<int>b(101,0);
-        vector<int>c(101,0);
-        
-        
-        for(auto i: nums1){
-            a[i]++;
-        }
-        
-        for(auto i: nums2){
-            b[i]++;
-        }
-        
-        for(auto i: nums3){
-            c[i]++;
-        }
+       for(auto i: nums1) a[i]=a[i]|1<<0;
+        for(auto i: nums2) a[i]=a[i]|1<<1;
+        for(auto i: nums3) a[i]=a[i]|1<<2;
         vector<int>ans;
-        for(int i=0;i<101;i++){
-            if(a[i]&& b[i]||b[i]&&c[i]||a[i]&&c[i]){
-                ans.push_back(i);
-            }
-        }
+       for (int i = 1; i <= 100; i++) if (a[i] == 3 || a[i] >= 5) ans.push_back(i);
+        
         return ans;
     }
 };
