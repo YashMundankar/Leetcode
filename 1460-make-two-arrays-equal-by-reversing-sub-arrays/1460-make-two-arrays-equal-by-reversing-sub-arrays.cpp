19a86c57aae88b23faf1b89da18e8d016e5ade25
Yash Mundankar <90042941+YashMundankar@users.noreply.github.com>
2022-01-10 18:15:12 +0530

    Time: 12 ms (75.97%), Space: 14.8 MB (43.41%) - LeetHub

--- 1460-make-two-arrays-equal-by-reversing-sub-arrays/1460-make-two-arrays-equal-by-reversing-sub-arrays.cpp
+++ 1460-make-two-arrays-equal-by-reversing-sub-arrays/1460-make-two-arrays-equal-by-reversing-sub-arrays.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     bool canBeEqual(vector<int>& target, vector<int>& arr) {
-        vector<int>a(1001,0);
-        vector<int>b(1001,0);
+        vector<int>a(1001,-1);
         for(auto i : target) a[i]++;
-        for(auto i : arr) b[i]++;
+        for(auto i : arr) a[i]--;
         for(int i=1;i<1001;i++){
-            if(a[i]!=b[i]){
+            if(a[i]!=0 && a[i]!=-1){
                 return false;
             }
         }
