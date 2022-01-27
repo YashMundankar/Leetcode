ef8053f714cf14f9837c896da04ca5beffcd2262
Yash Mundankar <90042941+YashMundankar@users.noreply.github.com>
2022-01-27 19:33:36 +0530

    Time: 8 ms (6.63%), Space: 6.1 MB (98.76%) - LeetHub

--- 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.cpp
+++ 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.cpp
@@ -1,32 +1,15 @@
 class Solution {
 public:
     
-    bool isNumber(string s)
-    {
-    for (int i = 0; i < s.length(); i++)
-        if (isdigit(s[i]) == false)
-            return false;
- 
-    return true;
-    }
-    
     bool areNumbersAscending(string s) 
     {
-        vector<int>ans;
         stringstream ss(s);
         string token;
-        while(getline(ss,token,' ')){
-            
-            if(isNumber(token)){
-                int val=stoi(token);
-                ans.push_back(val);
-                //cout<<token<<" ";
-            }
-        }
-        
-        for(int i=1;i<ans.size();i++){
-            if(ans[i]<=ans[i-1]){
-                return false;
+        int last_seen=0;
+        while(ss>>token){
+            if(isdigit(token[0])){
+                if(stoi(token)<=last_seen) return false;
+                last_seen=stoi(token);
             }
         }
         return true;
