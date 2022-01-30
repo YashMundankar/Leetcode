328cd2f6ebf0bc67ecb14beda23d1f6345710759
Yash Mundankar <90042941+YashMundankar@users.noreply.github.com>
2022-01-30 18:13:05 +0530

    Time: 5 ms (26.26%), Space: 10.9 MB (76.47%) - LeetHub

--- 993-cousins-in-binary-tree/993-cousins-in-binary-tree.cpp
+++ 993-cousins-in-binary-tree/993-cousins-in-binary-tree.cpp
@@ -12,20 +12,27 @@
 class Solution {
 public:
     
-   void  traverse(TreeNode* root,TreeNode* parent, int lvl,int x, int y,map<int,pair<TreeNode*,int>>&m1){
+void  traverse(TreeNode* root,TreeNode* parent,int lvl,int x,int &xDepth,TreeNode* &xParent,int y,int &yDepth,TreeNode* &yParent){
         if(root==NULL) return ;
-        if(root->val==x) m1[x]={parent,lvl};
-        if(root->val==y) m1[y]={parent,lvl};
-        traverse(root->left,root,lvl+1,x,y,m1);
-        traverse(root->right,root,lvl+1,x,y,m1);
+        if(root->val==x){
+            xDepth=lvl;
+            xParent=parent;
+        }
+        if(root->val==y){
+             yDepth=lvl;
+             yParent=parent;
+            
+        }
+        traverse(root->left,root,lvl+1,x,xDepth,xParent,y,yDepth,yParent);
+        traverse(root->right,root,lvl+1,x,xDepth,xParent,y,yDepth,yParent);
     }
     
     bool isCousins(TreeNode* root, int x, int y) {
         if(root==NULL) return false;
-        map<int,pair<TreeNode*,int>>m1;
-        int lvl=0; 
-        traverse(root,NULL,lvl,x,y,m1);
-        if(m1[x].first!=m1[y].first && m1[x].second==m1[y].second) return true;
-        return false;
+        int xDepth=-1,yDepth=-1;
+        TreeNode* xParent=NULL;
+        TreeNode* yParent=NULL;
+        traverse(root,NULL,0,x,xDepth,xParent,y,yDepth,yParent);
+        return (xDepth==yDepth && xParent!=yParent);
     }
 };
