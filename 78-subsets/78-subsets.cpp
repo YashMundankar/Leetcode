26358b92cfc59ec488aca91a708d676930d8ad3a
Yash Mundankar <90042941+YashMundankar@users.noreply.github.com>
2022-02-13 09:21:12 +0530

    Time: 10 ms (11.76%), Space: 16.9 MB (8.23%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     
-    void recurrsive(vector<vector<int>>&s1,vector<int>nums,vector<int>op){
-        if(nums.size()==0){
+    void recurrsive(vector<vector<int>>&s1,vector<int>nums,vector<int>op,int i){
+        if(i==nums.size()){
             s1.push_back(op);
             return;
         }
-        int temp=nums[0];
-        auto nums2=vector<int>(nums.begin()+1,nums.end());
-        recurrsive(s1,nums2,op);
+        int temp=nums[i];
+        //auto nums2=vector<int>(nums.begin()+1,nums.end());
+        recurrsive(s1,nums,op,i+1);
         op.push_back(temp);
-        recurrsive(s1,nums2,op);
+        recurrsive(s1,nums,op,i+1);
         
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>>s1;
         vector<int>op;
-        recurrsive(s1,nums,op);
+        recurrsive(s1,nums,op,0);
         return s1;
     }
 };
