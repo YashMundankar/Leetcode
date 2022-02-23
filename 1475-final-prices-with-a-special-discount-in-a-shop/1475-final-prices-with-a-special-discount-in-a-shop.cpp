9ab9bf413286ee79bcbf2a841935d1cd502bc1ef
Yash Mundankar <90042941+YashMundankar@users.noreply.github.com>
2022-02-23 23:36:11 +0530

    Time: 11 ms (22.04%), Space: 10 MB (17.05%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
+        stack<int>s;
         for(int i=0;i<prices.size();i++){
-            int flag=1;
-            for(int j=i+1;j<prices.size();j++){
-                if(prices[i]>=prices[j] && flag) {prices[i]-=prices[j];flag=0;}
+            while(!s.empty() && prices[s.top()]>=prices[i]){
+                prices[s.top()]-=prices[i];
+                s.pop();
             }
+            s.push(i);
         }
         return prices;
         
