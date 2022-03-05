8ba6055cb5f6eb86f37b62f194a519219068a51a
Yash Mundankar <90042941+YashMundankar@users.noreply.github.com>
2022-03-05 19:49:01 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (31.23%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.cpp
+++ 165-compare-version-numbers/165-compare-version-numbers.cpp
@@ -3,40 +3,23 @@ public:
 
     
     int compareVersion(string version1, string version2) {
-        vector<int>s1;
-        vector<int>s2;
-        stringstream ss(version1);
-        stringstream ss2(version2);
-        string token;
-        while(getline(ss,token,'.'))
-        {
-            s1.push_back(stoi(token));
-        }
-        while(getline(ss2,token,'.'))
-        {
-            s2.push_back(stoi(token));
-        }
-        if(s1.size()<s2.size()){
-            int k=s2.size()-s1.size();
-            while(k--) s1.push_back(0);
-        }
-        else if(s1.size()>s2.size()){
-            int k=s1.size()-s2.size();
-            while(k--) s2.push_back(0);
-        }
-        
-        int i=0;
-        while(i<s1.size()){
-            if(s1[i]<s2[i]) return -1;
-            else if(s1[i]>s2[i]) return 1;
+        int i=0,j=0,n1=0,n2=0;
+        while(i<version1.length()|| j<version2.length()){
+            n1=0,n2=0;
+            while(i<version1.length()&&version1[i]!='.'){
+                n1=(n1*10)+(version1[i]-'0');
+                i++;
+            }
+            while(j<version2.length()&&version2[j]!='.'){
+                n2=(n2*10)+(version2[j]-'0');
+                j++;
+            }
+            if(n1<n2) return -1;
+            else if(n1>n2) return 1;
             i++;
+            j++;
+            
         }
-        return 0;
-        
-        //for(auto i: s1) cout<<i<<"  ";
-        //cout<<endl;
-        //for(auto i: s2) cout<<i<<"  ";
-        
         return 0;
     }
 };
