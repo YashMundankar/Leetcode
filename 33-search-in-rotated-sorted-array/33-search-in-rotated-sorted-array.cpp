bace396473ea1afbc8e794bbc45d8928be9666f1
Yash Mundankar <90042941+YashMundankar@users.noreply.github.com>
2022-03-28 23:31:19 +0530

    Time: 8 ms (33.81%), Space: 11.4 MB (29.80%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,5 +1,26 @@
 class Solution {
 public:
+    
+    int binary_search(int start ,int end, int target, vector<int>arr){
+        int ans=-1;
+         while(start<=end){
+	        int mid=start+(end-start)/2;
+	        if(arr[mid]==target){
+	            ans=mid;
+                break;
+	        }
+	        else if(arr[mid]<target){
+	            start=mid+1;
+	        }
+	        else if(arr[mid]>target){
+	            end=mid-1;
+	        }
+         }
+        return ans;
+        
+    }
+    
+    
     int search(vector<int>& arr, int target) {
       int start=0,end=arr.size()-1;
        int n=arr.size(),indx=0;
@@ -17,40 +38,13 @@ public:
 	        else if(arr[mid]>=arr[start]){
 	            start=mid+1;
 	        }
+	    
+       
 	    }
-        start=0;
-        end=indx;
         int ans=-1;
-         while(start<=end){
-	        int mid=start+(end-start)/2;
-	        if(arr[mid]==target){
-	            ans=mid;
-                break;
-	        }
-	        else if(arr[mid]<target){
-	            start=mid+1;
-	        }
-	        else if(arr[mid]>target){
-	            end=mid-1;
-	        }
-	    }
+        ans=binary_search(0,indx,target,arr);
         if(ans==-1){
-            start=indx;
-        end=n-1;
-         while(start<=end){
-	        int mid=start+(end-start)/2;
-	        if(arr[mid]==target){
-	            ans=mid;
-                break;
-	        }
-	        else if(arr[mid]<target){
-	            start=mid+1;
-	        }
-	        else if(arr[mid]>target){
-	            end=mid-1;
-	        }
-	    }
-            
+            ans=binary_search(indx,n-1,target,arr);
         }
         return ans;
 	    
