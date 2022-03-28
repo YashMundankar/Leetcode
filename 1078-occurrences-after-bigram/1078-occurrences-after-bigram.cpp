b5dca798c23fbef986746a2a9235bc1817d82769
Yash Mundankar <90042941+YashMundankar@users.noreply.github.com>
2022-03-28 15:31:10 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (73.28%) - LeetHub

--- 1078-occurrences-after-bigram/1078-occurrences-after-bigram.cpp
+++ 1078-occurrences-after-bigram/1078-occurrences-after-bigram.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
     vector<string> findOcurrences(string text, string first, string second) {
-        stringstream ss(text);
-        string word;
-        vector<string>ans;
-        vector<string>temp;
-        
-        while(ss>>word){
-            temp.push_back(word);
-        }
-        int i=0,j=1;
-        while(i<temp.size()-2){
-            if(temp[i]==first && temp[j]==second){
-                ans.push_back(temp[j+1]);
-            }
-            i++;
-            j++;
+        vector<string> ans;
+        istringstream ss(text);
+        string prev2, prev, word;
+        while (ss >> word) {
+            if (prev2 == first && prev == second) ans.push_back(word);
+            prev2 = prev;
+            prev = word;
         }
         return ans;
     }
