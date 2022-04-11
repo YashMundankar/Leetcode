646c3478f2bf29d7fcf5bcb08aa9218efd3f9e6b
Yash Mundankar <90042941+YashMundankar@users.noreply.github.com>
2022-04-11 19:18:15 +0530

    Time: 31 ms (74.64%), Space: 14 MB (73.22%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -6,9 +6,9 @@ public:
         vector<vector<int>>soln(m,vector<int>(n,0));
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                int one_d_index=((i*n)+j+k)%(m*n);
-                int new_i=one_d_index/n;
-                int new_j=one_d_index%n;
+                //int one_d_index=((i*n)+j+k)%(m*n);
+                int new_j=(j+k)%n ;
+                int new_i=(i+((j+k)/n))%m;
                 soln[new_i][new_j]=grid[i][j];
                 
             }
