7f3ebf19c887fddcaa291401fb160a60118cc4af
Yash Mundankar <90042941+YashMundankar@users.noreply.github.com>
2022-04-28 23:49:46 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (62.86%) - LeetHub

---
+++ 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int secondHighest(string s) {
+        int first=INT_MIN,second=INT_MIN;
+        for(auto i: s){
+            int val=i-'0';
+            if(val>=0 && val<=9){
+                if(val>first){
+                    second=first;
+                    first=val;
+                }
+                else if(val>second && val!=first){
+                    second=val;
+                }
+            }
+        }
+        cout<<first<<" "<<second<<endl;
+        if(first==INT_MIN||second==INT_MIN) return -1;
+        return second;
+        
+    }
+};
